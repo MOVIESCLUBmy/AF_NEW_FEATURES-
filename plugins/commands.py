fd98657b8ef87c0bc4dfa1075ee9ad6565da0531
MOVIESCLUB MY <98048092+MOVIESCLUBmy@users.noreply.github.com>
2022-05-31 11:38:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('💠 GᏒԾUᎮ 💠', url='https://t.me/MMOTTMOVIES')
+                InlineKeyboardButton('🎭 GᏒԾUᎮ 🎭', url='https://t.me/MOVIES_CLUB_GROUP_2')
             ],
             [
                 InlineKeyboardButton('♻️ HΞLᎮ ♻️', url=f"https://t.me/{temp.U_NAME}?start=help")
@@ -44,8 +44,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 GᏒԾUᎮ 💠', url='https://t.me/MMOTTMOVIES'),
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MMOTTMOVIE')         
+            InlineKeyboardButton('🎭 GᏒԾUᎮ 🎭', url='https://t.me/MOVIES_CLUB_GROUP_2'),
+            InlineKeyboardButton('📀 CHΛИИΞL 📀', url='https://t.me/MOVIESCLUB_OTT_UPDATES')        
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
@@ -87,8 +87,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 GᏒԾUᎮ 💠', url='https://t.me/MMOTTMOVIES'),
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MMOTTMOVIE')         
+            InlineKeyboardButton('🎭 GᏒԾUᎮ 🎭', url='https://t.me/MOVIES_CLUB_GROUP_2'),
+            InlineKeyboardButton('📀 CHΛИИΞL 📀', url='https://t.me/MOVIESCLUB_OTT_UPDATES')         
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
